1bea4bf18357f92bacbc7df27afeb1220ef598a6
leonex-Host <leonex-host@example.com>
2026-09-24 10:55:09 +0530

    style(extension): Update notification badge hex color to a highly visible Orange-Red blend per user request

--- extension/core/job-manager.js
+++ extension/core/job-manager.js
@@ -148,7 +148,7 @@ async function syncMapToStorage() {
 
     if (pausedCount > 0) {
         chrome.action.setBadgeText({ text: String(pausedCount) }).catch(() => { });
-        chrome.action.setBadgeBackgroundColor({ color: '#ef4444' }).catch(() => { });
+        chrome.action.setBadgeBackgroundColor({ color: '#FF4500' }).catch(() => { });
         notifyActiveTabBell(true);
     } else {
         chrome.action.setBadgeText({ text: '' }).catch(() => { });
